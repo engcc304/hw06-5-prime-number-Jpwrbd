9103687c1e038d72b82f4cf59d4b05ccca745b3b
Jpwrbd <jpwrbd456@gmail.com>
2023-08-04 02:03:46 +0700

    test1000000

--- Homework/HW06/hw06-5-prime-number.cpp
+++ Homework/HW06/hw06-5-prime-number.cpp
@@ -15,7 +15,6 @@
         97 89 83 79 73 71 67 61 59 53 47 43 41 37 31 29 23 19 17 13 11 7 5 3 2
 */
 #include <stdio.h>
-
 int isPrime(int num)
 {
     if (num <= 1)
@@ -27,14 +26,12 @@ int isPrime(int num)
     }
     return 1;
 }
-
 int main()
 {
     int n;
     printf("Test case: \n");
-    printf("    Enter number: ");
+    printf("Enter number: ");
     scanf("%d", &n);
-
     printf("Output: \n");
     for (int i = n; i >= 2; i--)
     {
@@ -47,6 +44,5 @@ int main()
             }
         }
     }
-
     return 0;
 }
